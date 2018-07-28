28e40872a4630c37b4c3a1b5a03af99485567150
David Turner <David.Turner@invitationhomes.com>
2018-07-28 11:40:29 -0700

    R

---
+++ I dont git it.R
@@ -0,0 +1,20 @@
+
+
+
+install.packages('DataExplorer')
+library(DataExplorer)
+
+
+# load training data
+train <- read.csv("C:/Users/david.turner.CORP/Desktop/Projects/GIT/krolak_and_turner/train.csv")
+
+# load testing data
+test <- read.csv("C:/Users/david.turner.CORP/Desktop/Projects/GIT/krolak_and_turner/test.csv")
+
+
+# meh
+create_report(train)
+
+
+
+
